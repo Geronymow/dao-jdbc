5d3c6cb6cb808bc7cdd1157053c57794a87511d5
Guilherme Geronymo <guilherme.geronymo@gmail.com>
2025-01-10 17:13:43 -0300

    Criando Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,23 @@
-package application;public class Program {
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+
+        Seller sl = new Seller(14,"Guilherme","guilherme@gmail.com",
+                sdf.parse("18/12/2004"), 2000.0, new Department(14,"Boxes"));
+
+        System.out.println(sl.toString());
+
+
+
+    }
 }
