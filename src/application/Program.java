37c0620f7889593eca45088834263bb1e064b0f6
Guilherme Geronymo <guilherme.geronymo@gmail.com>
2025-01-11 10:51:00 -0300

    FindAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,9 +16,17 @@ public class Program {
         System.out.println("=== TEST 1: Seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        System.out.println();
 
         System.out.println("=== TEST 2: Seller findByDepartment ===");
         List<Seller> list = sellerDao.findByDepartment(new Department(2,null));
+        for(Seller obj : list) {
+            System.out.println(obj);
+        }
+        System.out.println();
+
+        System.out.println("=== TEST 3: Seller findAll ===");
+        list = sellerDao.findAll();
         for(Seller obj : list) {
             System.out.println(obj);
         }
